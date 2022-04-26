02811c14524ebd3c1ccac1488db818913f4e0f24
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 14:16:00 -0700

    add query param to ensure only images are queried

--- src/api/searchNASAImageAPI.js
+++ src/api/searchNASAImageAPI.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 // queries NASA Image and Video Library API (nasaIVLAPI) with specified query string
 const searchNASAImageAPI = async (query) =>
-  await axios.get(`https://images-api.nasa.gov/search?q=${query}`);
+  await axios.get(
+    `https://images-api.nasa.gov/search?q=${query}&media_type=image`
+  );
 
 export default searchNASAImageAPI;
