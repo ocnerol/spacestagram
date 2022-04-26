3a167f4e3285c345cf19619452210497cec6d340
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 15:26:34 -0700

    map over searchResults and make each a SpaceImage component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,15 @@
 import React from "react";
+import SpaceImage from "./SpaceImage";
 
-export default function Feed({ searchItems }) {
+export default function Feed({ searchResults }) {
+  const feedItems = searchResults.map((item, index) => (
+    <SpaceImage
+      key={index}
+      description={item.description}
+      photoURI={item.photoURI}
+      title={item.title}
+      dateCreated={item.dateCreated}
+    />
+  ));
   return;
 }
