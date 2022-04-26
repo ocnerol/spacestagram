05d8c4ae39f51e1da112d79b94f94a5f26336e25
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 12:52:31 -0700

    ensure there are search result items in result object

--- src/api/nasaIVLAPI.test.js
+++ src/api/nasaIVLAPI.test.js
@@ -16,4 +16,22 @@ describe("NASA Image and Video Library API search method", () => {
     const result = await searchNASAImageAPI("space");
     expect(result.data).toEqual(mockResult.data);
   });
+
+  it("items array property in collection object has 100 items when searching for 'moon'", async () => {
+    const oneHundredItems = new Array(100);
+    const mockResult = {
+      data: {
+        collection: {
+          items: oneHundredItems,
+        },
+      },
+    };
+
+    axios.get.mockResolvedValue(mockResult);
+
+    const result = await searchNASAImageAPI("moon");
+    expect(result.data.collection.items.length).toEqual(
+      mockResult.data.collection.items.length
+    );
+  });
 });
