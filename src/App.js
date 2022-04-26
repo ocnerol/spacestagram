6b7556f8e4bd475d16e00838a537c94d1aed4998
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 16:31:50 -0700

    add Feed and pass searchResults - remove console log

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import Feed from "./components/Feed";
 import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
-  console.log({ searchResults });
   return (
     <div className="App">
       <SearchBar setSearchResults={setSearchResults} />
+      <Feed searchResults={searchResults} />
     </div>
   );
 }
