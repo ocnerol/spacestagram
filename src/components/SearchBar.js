3d6fa936e98789d9c3e9a45567bfd135c4dcfb21
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 14:12:36 -0700

    design handleSubmit to call searchNASAImageAPI with search state variable

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import { TextField } from "@mui/material";
+import searchNASAImageAPI from "../api/searchNASAImageAPI";
 
 export default function SearchBar() {
   const [search, setSearch] = useState("");
+
   const handleChange = (e) => {
     const newChar = e.target.value;
     setSearch(newChar);
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await searchNASAImageAPI(search);
+    const { items } = data.collection;
+    console.log({ items });
+  };
   return (
-    <TextField
-      label="Search into space"
-      variant="filled"
-      value={search}
-      onChange={handleChange}
-    />
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Search into space"
+        variant="filled"
+        value={search}
+        onChange={handleChange}
+      />
+    </form>
   );
 }
