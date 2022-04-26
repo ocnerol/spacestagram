92fc37c59df4b3788d3fd27ae25f65d983757151
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 16:31:17 -0700

    use an ImageList component to render the feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,3 +1,4 @@
+import { ImageList } from "@mui/material";
 import React from "react";
 import SpaceImage from "./SpaceImage";
 
@@ -5,11 +6,14 @@ export default function Feed({ searchResults }) {
   const feedItems = searchResults.map((item, index) => (
     <SpaceImage
       key={index}
-      description={item.description}
-      photoURI={item.photoURI}
+      photoURI={item.originalImageURI}
       title={item.title}
       dateCreated={item.dateCreated}
     />
   ));
-  return;
+  return (
+    <ImageList variant="masonry" cols={3} gap={8}>
+      {feedItems}
+    </ImageList>
+  );
 }
