fa846fdbc5f2e77691a9c8dedf83aac612b55046
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 12:49:25 -0700

    ensure function returns an object with only a 'collection' property

--- src/api/nasaIVLAPI.test.js
+++ src/api/nasaIVLAPI.test.js
@@ -1,4 +1,19 @@
-import nasaIVLAPI from "./nasaIVLAPI";
+import searchNASAImageAPI from "./nasaIVLAPI";
 import axios from "axios";
 
 jest.mock("axios");
+
+describe("NASA Image and Video Library API search method", () => {
+  it("returns an object with a single 'collection' property", async () => {
+    const mockResult = {
+      data: {
+        collection: {},
+      },
+    };
+
+    axios.get.mockResolvedValue(mockResult);
+
+    const result = await searchNASAImageAPI("space");
+    expect(result.data).toEqual(mockResult.data);
+  });
+});
