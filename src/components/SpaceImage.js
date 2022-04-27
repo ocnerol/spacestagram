bd24d77fb308df933b048f1bdfa6a841d19b4c3d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 17:16:20 -0700

    add footer to each image in feed

--- src/components/SpaceImage.js
+++ src/components/SpaceImage.js
@@ -1,10 +1,17 @@
-import { ImageListItem } from "@mui/material";
+import { ImageListItem, ImageListItemBar } from "@mui/material";
 import React from "react";
+import LikeButton from "./LikeButton";
 
 export default function SpaceImage({ photoURI, title, dateCreated }) {
   return (
     <ImageListItem>
       <img src={photoURI} alt={title} />
+      <ImageListItemBar
+        position="bottom"
+        title={title}
+        subtitle={dateCreated}
+        actionIcon={<LikeButton />}
+      />
     </ImageListItem>
   );
 }
