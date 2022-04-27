aa666eccde89dd92b9956c715a457102b12b90cc
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 17:47:46 -0700

    fix header to top

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
+
 import AppTitle from "../AppTitle";
 import SearchBar from "../SearchBar/SearchBar";
+import "./Header.css";
 
 export default function Header({ setSearchResults }) {
   return (
-    <div>
+    <div className="header">
       <AppTitle />
       <SearchBar setSearchResults={setSearchResults} />
     </div>
