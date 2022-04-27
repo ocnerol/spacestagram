4042f7670fce465d276472aac0a0e3da61c1bfec
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 18:50:49 -0700

    use id to be more specific

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AppTitle() {
   return (
-    <div>
+    <div id="app-title">
       <h1>Spacestagram</h1>
       <p>Journey into the universe</p>
     </div>
