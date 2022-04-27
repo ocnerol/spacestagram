86a266f3e142d79dc5c55b78616bf6689c085c61
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 18:32:23 -0700

    add Spiritualized placeholder artwork

---
+++ src/components/SpiritualizedPlaceholder.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SpiritualizedPlaceholder() {
+  return (
+    <div className="main spiritualizedArtWorkContainer">
+      <img
+        className="spiritualizedArtWork"
+        alt="Circle art from Spiritualized's album cover for the album Ladies and Gentlemen We Are Floating In Space"
+        src={require("../assets/spiritualized.jpg")}
+      ></img>
+    </div>
+  );
+}
