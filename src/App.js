b54b257611f2c2bb6050185ba5927ba38356803a
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-26 17:48:35 -0700

    remove unused import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import "./App.css";
 import Feed from "./components/Feed";
 import Header from "./components/Header/Header";
-import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
