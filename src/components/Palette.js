21ab7f5b05da3f08bf86d8ccb0c2548590bffacd
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-27 21:38:29 -0700

    add new palette theme object

---
+++ src/components/Palette.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: "#B399D4",
+    secondary: "#F5E1FD",
+    tertiary: "#CE9DD9",
+  },
+});
