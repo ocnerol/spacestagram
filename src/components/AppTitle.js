f1539718f86b4c30fa5675dfb802a2c9b85f5430
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-27 22:56:37 -0700

    enlarge app title and subtitle

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function AppTitle() {
   return (
     <div id="app-title">
-      <h1>Spacestagram</h1>
-      <p>Journey into the universe</p>
+      <h1 id="title-heading">Spacestagram</h1>
+      <p id="app-subtitle">Journey into the universe</p>
     </div>
   );
 }
