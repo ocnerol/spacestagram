7c411239af56e344e30c538b7c52276931a9a9c3
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-04-27 22:39:59 -0700

    use purple for like button colour

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -4,6 +4,7 @@ import {
   Favorite as FavoriteIcon,
   FavoriteBorder as FavoriteBorderIcon,
 } from "@mui/icons-material";
+import Palette from "./Palette";
 
 export default function LikeButton() {
   const [liked, setLiked] = useState(false);
@@ -13,12 +14,14 @@ export default function LikeButton() {
   };
 
   return (
-    <IconButton onClick={handleClick}>
-      {liked ? (
-        <FavoriteIcon color="error" />
-      ) : (
-        <FavoriteBorderIcon color="error" />
-      )}
-    </IconButton>
+    <Palette>
+      <IconButton onClick={handleClick}>
+        {liked ? (
+          <FavoriteIcon color="darkerPurple" />
+        ) : (
+          <FavoriteBorderIcon color="darkerPurple" />
+        )}
+      </IconButton>
+    </Palette>
   );
 }
